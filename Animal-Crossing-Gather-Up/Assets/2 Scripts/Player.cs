ce785b6f3fe38dd5c6ca696801ae1fb758387756
JiwonEE2 <jiwonchichi1@gmail.com>
2024-11-27 10:05:28 +0900

    [변경] Player
    test를 위해 변경된 player

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Player.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Player.cs
@@ -4,100 +4,112 @@ using static UnityEditor.Progress;
 
 public class Player : MonoBehaviour
 {
-    private CharacterController characterController;
-    public float moveSpeed = 5f;
-    public Transform handPosition; 
-    public GameObject equippedTool;
-    //public Inventory inventory; 
-    //public int money;
+	private CharacterController characterController;
+	public float moveSpeed = 5f;
+	public Transform handPosition;
+	public GameObject equippedTool;
 
-	//public Inventory inventory; // �÷��̾��� �κ��丮
-	//public int money; // ���� ���� ��
+	public delegate void ItemCollectedHandler(Item item);
+	public event ItemCollectedHandler OnItemCollected;
 
-    public delegate void ItemCollectedHandler(Item item);
-    public event ItemCollectedHandler OnItemCollected;
+	private ICollectCommand _currentCommand;
 
-     private ICollectCommand _currentCommand;
+	public void SetCommand(ICollectCommand command) { _currentCommand = command; }
+	public void Collect() => _currentCommand?.Execute();
 
-    public void SetCommand(ICollectCommand command) { _currentCommand = command; }
-    public void Collect() => _currentCommand?.Execute();
-
-    private void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-        OnItemCollected += AddItem;
-    }
-
-    private void Update()
-    {
-        HandleMovement();
-        HandleCollection();
-    }
-
-    private void HandleMovement()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-		// test
-		Debug.Log($"Collected {item.itemName}!");
+	private void Start()
+	{
+		characterController = GetComponent<CharacterController>();
 	}
 
-    private void HandleCollection()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Collect();
-        }
-    }
+	private void Update()
+	{
+		HandleMovement();
+		HandleCollection();
+		Test();
+	}
 
-    public void EquipTool(GameObject tool)
-    {
-        if (equippedTool != null)
-        {
-            UnequipTool();
-        }
+	// test of input item Player to Inventory
+	public Item i0;
+	public Item i1;
+	public Item t0;
+	public Item t1;
+	private void Test()
+	{
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			CollectItem(i0);
+		}
+		else if (Input.GetKeyDown(KeyCode.X))
+		{
+			CollectItem(i1);
+		}
+		else if (Input.GetKeyDown(KeyCode.C))
+		{
+			CollectItem(t0);
+		}
+		else if (Input.GetKeyDown(KeyCode.V))
+		{
+			CollectItem(t1);
+		}
+	}
 
-        equippedTool = tool;
-        equippedTool.transform.SetParent(handPosition);
-        equippedTool.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
-    }
+	private void HandleMovement()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+	}
 
-    // ���?����
-    public void UnequipTool()
-    {
-        if (equippedTool != null)
-        {
-            //inventory.AddItem(equippedTool); 
-            equippedTool.transform.SetParent(null);
-            equippedTool.SetActive(false);
-            equippedTool = null;
-        }
-    }
+	private void HandleCollection()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Collect();
+		}
+	}
 
-    public void CollectItem(Item item)
-    {
-        //OnItemCollected?.Invoke(item);
-        //invetory.OnItemCollected?.Invoke(item);
-        //Debug.Log($"Collected {item.itemName}!");
-    }
+	public void EquipTool(GameObject tool)
+	{
+		if (equippedTool != null)
+		{
+			UnequipTool();
+		}
 
-    public void AddItem(Item item)
-    {
-        //inventory.AddItem(item);
-    }
+		equippedTool = tool;
+		equippedTool.transform.SetParent(handPosition);
+		equippedTool.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+	}
 
-    // ä���� �Ǹ�
-    public void SellItem(GameObject item)
-    {
-        //if (inventory.RemoveItem(item))
-        //{
-        //    //money += item.sellPrice;
-        //    //Debug.Log($"Sold {item.name} for {item.sellPrice}. Current Money: {money}");
-        //}
-        //else
-        //{
-        //    Debug.Log("Item not found in inventory.");
-        //}
-    }
+	// ���?����
+	public void UnequipTool()
+	{
+		if (equippedTool != null)
+		{
+			//inventory.AddItem(equippedTool); 
+			equippedTool.transform.SetParent(null);
+			equippedTool.SetActive(false);
+			equippedTool = null;
+		}
+	}
+
+	public void CollectItem(Item item)
+	{
+		OnItemCollected?.Invoke(item);
+		//invetory.OnItemCollected?.Invoke(item);
+		//Debug.Log($"Collected {item.itemName}!");
+	}
+
+	// ä���� �Ǹ�
+	public void SellItem(GameObject item)
+	{
+		//if (inventory.RemoveItem(item))
+		//{
+		//    //money += item.sellPrice;
+		//    //Debug.Log($"Sold {item.name} for {item.sellPrice}. Current Money: {money}");
+		//}
+		//else
+		//{
+		//    Debug.Log("Item not found in inventory.");
+		//}
+	}
 }
