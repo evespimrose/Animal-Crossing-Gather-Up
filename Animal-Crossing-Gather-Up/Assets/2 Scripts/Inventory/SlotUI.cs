51f999c1baeef0d77546e250f50adf86eb40115b
JiwonEE2 <jiwonchichi1@gmail.com>
2024-11-28 18:44:07 +0900

    [변경] SlotUI
    말풍선에서 아이템 이름 출력되도록 수정

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Inventory/SlotUI.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Inventory/SlotUI.cs
@@ -9,11 +9,13 @@ public class SlotUI : MonoBehaviour
 	public Image itemImage; // Reference to the UI Image for the item
 	public TextMeshProUGUI stackCountText;  // Reference to the UI Text for the stackCount
 	public Image choiceBackground;  // Reference to the choice background image
+	public TextMeshProUGUI itemNameText;
 
 	public void UpdateUI(Item item, int stackCount)
 	{
 		if (item != null)
 		{
+			itemNameText.text = item.name;
 			itemImage.gameObject.SetActive(true);
 			itemImage.sprite = item.icon;   // Set the item icon
 			stackCountText.text = stackCount > 1 ? stackCount.ToString() : "";  // Show stackCount if greater than 1
