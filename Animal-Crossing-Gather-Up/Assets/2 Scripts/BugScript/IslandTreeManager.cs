64b92ed7496505147f179f9a5eff62384f99f88e
RJH7387 <abj2563@naver.com>
2024-11-28 20:39:39 +0900

    [변경] Prefab/Bug
    삭제입니다

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/BugScript/IslandTreeManager.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/BugScript/IslandTreeManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class IslandTreeManager : MonoBehaviour
@@ -8,10 +9,56 @@ public class IslandTreeManager : MonoBehaviour
     [SerializeField] private int maxBugsInIsland = 3;// 섬의 최대 생성 될 수 있는 벌레 한계치 
     [SerializeField] private string islandName;      // 각 섬의 이름
 
+    private List<TreebugSpawner> treeSpawners = new List<TreebugSpawner>();
     private int currentBugCount = 0; // 현재 벌레의 수
 
+    private void Start()
+    {
+        treeSpawners.AddRange(GetComponentsInChildren<TreebugSpawner>());
+
+        if (treeSpawners.Count == 0) return;
+
+        //시작 코루틴
+        StartCoroutine(SpawnBugRoutine());
+    }
+
+    private IEnumerator SpawnBugRoutine()
+    {
+        while (true)
+        {
+            if (CanSpawnBug())
+            {
+                SpawnBugOnRandomTree();
+            }
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    private void SpawnBugOnRandomTree()
+    {
+        //나무를 담을 임시 리스트
+        List<TreebugSpawner> availableTrees = new List<TreebugSpawner>();
+
+        //벌레 없는 찾기
+        for (int i = 0; i < treeSpawners.Count; i++)
+        {
+            if (treeSpawners[i].currentBug == null)
+            {
+                availableTrees.Add(treeSpawners[i]);
+            }
+        }
+        if (availableTrees.Count > 0)
+        {
+            int randomIndex = Random.Range(0, availableTrees.Count);
+            TreebugSpawner selectedTree = availableTrees[randomIndex];
+            selectedTree.SpawnRandomBug();
+        }
+    }
+
+
+
     //생성이 가능 한지 확인
-    private bool CanSpawnBug()
+    public bool CanSpawnBug()
     {
         return currentBugCount < maxBugsInIsland;
     }
--- Animal-Crossing-Gather-Up/Assets/2 Scripts/BugScript/Treebugspawner.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/BugScript/Treebugspawner.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Treebugspawner : MonoBehaviour
+public class TreebugSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<BugPrefabData> bugPrefab;
+    [SerializeField] private float spawnInterval = 10f; //스폰 간격
+    [SerializeField] private Transform spawnPoint; //드래그엔 드롭으로 위치 지정
+
+    private IslandTreeManager IslandManager;
+    public Bug currentBug;
+
+    private void Start() 
     {
-        
+          
+        IslandManager = GetComponentInParent<IslandTreeManager>();
+        if (IslandManager == null) return;       
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    public void SpawnRandomBug()
+    { 
+        if(bugPrefab.Count == 0) return;
+
+        //랜덤으로 벌레 선택
+        int randomIndex = Random.Range(0, bugPrefab.Count);
+        BugPrefabData selectedBug = bugPrefab[randomIndex];
+
+      
+        //지정된 위쳉 벌레 생성
+        GameObject newBugObject = Instantiate(selectedBug.prefab, spawnPoint.position, selectedBug.prefab.transform.rotation);
+        newBugObject.transform.SetParent(spawnPoint, worldPositionStays: true);
+
+        currentBug = newBugObject.GetComponent<Bug>();
+        IslandManager.AddBug();
+
     }
+
+
+
+
+
 }
