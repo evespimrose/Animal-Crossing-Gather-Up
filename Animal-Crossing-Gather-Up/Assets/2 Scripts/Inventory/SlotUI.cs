8fffc0ae6c772eecad15e10e9deb20c3e97a556c
JiwonEE2 <jiwonchichi1@gmail.com>
2024-11-28 17:47:56 +0900

    [변경] SlotUI
    키보드 입력에 따라 background 활성 비활성 구현

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Inventory/SlotUI.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Inventory/SlotUI.cs
@@ -8,6 +8,7 @@ public class SlotUI : MonoBehaviour
 {
 	public Image itemImage; // Reference to the UI Image for the item
 	public TextMeshProUGUI stackCountText;  // Reference to the UI Text for the stackCount
+	public Image choiceBackground;  // Reference to the choice background image
 
 	public void UpdateUI(Item item, int stackCount)
 	{
@@ -16,12 +17,18 @@ public class SlotUI : MonoBehaviour
 			itemImage.gameObject.SetActive(true);
 			itemImage.sprite = item.icon;   // Set the item icon
 			stackCountText.text = stackCount > 1 ? stackCount.ToString() : "";  // Show stackCount if greater than 1
+			choiceBackground.gameObject.SetActive(true);    // Ensure choice Background is hidden initialy
 		}
 		else
 		{
-			//itemImage.sprite = null;    // Clear the image if no item
-			itemImage.gameObject.SetActive(false);
+			itemImage.gameObject.SetActive(false);  // Hide the image if no item
 			stackCountText.text = "";   // Clear the stackCount
+			choiceBackground.gameObject.SetActive(false);   // Ensure choice backgournd is hidden
 		}
+	}
+
+	public void SelectSlot(bool isSelected)
+	{
+		choiceBackground.gameObject.SetActive(isSelected);  // Activate or deactivate choice background
 	}
 }
