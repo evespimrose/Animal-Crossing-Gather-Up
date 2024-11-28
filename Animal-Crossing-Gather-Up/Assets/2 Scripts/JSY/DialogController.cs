953637386785a40f38c254a5a2c94faea7f00f99
Soyoung <soyoung7896@naver.com>
2024-11-28 18:44:37 +0900

    [변경] DialogController
    대화창 엔터패널 추가

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/JSY/DialogController.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/JSY/DialogController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEditor.Experimental.GraphView;
+using Unity.VisualScripting;
 
 
 
@@ -11,10 +12,12 @@ public class DialogController : MonoBehaviour
 {
     public TextMeshProUGUI dialogText; //UIManager로 옮길 예정
     public GameObject choosePanel; //UIManager로 옮길 예정
+    public GameObject enterPaner;
 
     private Coroutine currentCoroutine;
     private NPCAreaController npctrl;
     private bool isChooseActive = false;
+    private bool isEnterActive = false;
     private int talkCount = 0;
 
     private void Start()
@@ -22,7 +25,9 @@ public class DialogController : MonoBehaviour
         npctrl = FindObjectOfType<NPCAreaController>();
         dialogText.text = "";
         choosePanel.SetActive(false);
+        enterPaner.SetActive(false);
     }
+
 
     public void DialogStart()
     {
@@ -36,6 +41,7 @@ public class DialogController : MonoBehaviour
         {
             if (talkCount == 1 && currentCoroutine == null)
             {
+                isEnterActive = false;
                 string text2 = "~고객님께 미소를 전하는 안전한 날개~\n도도항공의 탑승 수속 담당자 모리입니다!\n잘 부탁드립니다!";
                 currentCoroutine = StartCoroutine(TypingDialog(text2));
                 talkCount++;
@@ -43,6 +49,7 @@ public class DialogController : MonoBehaviour
 
             else if (talkCount == 2 && currentCoroutine == null)
             {
+                isEnterActive = false;
                 string text3 = "다른 섬 방문, 이 섬에 아는 분을 초대하고 싶을 때는\n저희 비행장을 이용해주세요!";
                 currentCoroutine = StartCoroutine(TypingDialog(text3));
                 talkCount++;
@@ -50,11 +57,22 @@ public class DialogController : MonoBehaviour
 
             else if (talkCount == 3 && currentCoroutine == null)
             {
+                isEnterActive = false;
                 string text4 = "고객님 앞으로 너굴 사장님이 마일 여행권을 맡겨두셨습니다!\n마일 여행권을 이용하고 싶으시면 제게 외출할래 라고 말씀해주세요";
                 currentCoroutine = StartCoroutine(TypingDialog(text4));
                 talkCount++;
                 isChooseActive = true;
             }
+        }
+
+        if (isEnterActive)
+        {
+            enterPaner.SetActive(true);
+        }
+
+        if (!isEnterActive)
+        {
+            enterPaner.SetActive(false);
         }
     }
 
@@ -78,6 +96,7 @@ public class DialogController : MonoBehaviour
             yield return new WaitForSeconds(0.1f);
         }
         currentCoroutine = null;
+        isEnterActive = true;
 
         if (isChooseActive)
         {
@@ -90,6 +109,7 @@ public class DialogController : MonoBehaviour
         Debug.Log("마일섬 출발");
         choosePanel.SetActive(false);
         npctrl.dialogPanel.SetActive(false);
+        enterPaner.SetActive(false);
     }
 
     public void EndTalk()
@@ -97,5 +117,6 @@ public class DialogController : MonoBehaviour
         Debug.Log("NPC 모리 대화 종료");
         choosePanel.SetActive(false);
         npctrl.dialogPanel.SetActive(false);
+        enterPaner.SetActive(false);
     }
 }
