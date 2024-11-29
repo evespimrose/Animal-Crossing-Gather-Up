e35685f03a3c51bc447932080808aeb7f3b8a3c7
Soyoung <soyoung7896@naver.com>
2024-11-29 19:49:32 +0900

    [변경] IAreaNPC
    IDialog -> IAreaNPC 이름 변경 및 스크립트 소폭 변경

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Interfaces/IAreaNPC.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Interfaces/IAreaNPC.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IDialog
+public interface IAreaNPC
 {
     public void FirstAccept(); //첫번째 선택지(선택지 3개일 경우 NPC에서 1개 추가)
     public void EndTalk(); //대화 종료 선택지
