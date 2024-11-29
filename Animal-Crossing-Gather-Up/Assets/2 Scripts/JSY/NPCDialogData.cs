bf244f9002fd782835144697f547edc50c5be6db
Soyoung <soyoung7896@naver.com>
2024-11-29 09:57:04 +0900

    [변경]NPCDialogData Script

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/JSY/NPCDialogData.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/JSY/NPCDialogData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NPCDialogData", menuName = "NPC/DialogData")]
 public class NPCDialogData : ScriptableObject
 {
-    public string npcName;
-    public string[] dialogTexts;
-    public bool isChooseActive;
+    public string npcName; //npc 이름
+    public string[] dialogTexts; //ncp 대화
+    public bool isChooseActive; //선택패널 활성화
 }
