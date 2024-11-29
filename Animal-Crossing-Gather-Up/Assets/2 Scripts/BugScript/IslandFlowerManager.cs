66b8739edf980890c2a6aa1d25c9ceea71e7c21a
RJH7387 <abj2563@naver.com>
2024-11-29 15:30:04 +0900

    [추가] Bug
    Bug script 안에 플라워 관련 스크립트랑 나비스폰 추가

---
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/BugScript/IslandFlowerManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandFlowerManager : MonoBehaviour
+{
+    [SerializeField] private int maxBugsInIsland = 3;
+    [SerializeField] private string islandName;
+
+    private List<FlowerBugSpawner> flowerSpawners = new List<FlowerBugSpawner>();
+    private int currentBugCount = 0;
+
+    private void Start()
+    {
+        flowerSpawners.AddRange(GetComponentsInChildren<FlowerBugSpawner>());
+        if (flowerSpawners.Count == 0) return;
+        StartCoroutine(SpawnBugRoutine());
+    }
+
+    private IEnumerator SpawnBugRoutine()
+    {
+        while (true)
+        {
+            if (CanSpawnBug())
+            {
+                SpawnBugOnRandomFlower();
+            }
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    private void SpawnBugOnRandomFlower()
+    {
+        List<FlowerBugSpawner> availableFlowers = new List<FlowerBugSpawner>();
+
+        for (int i = 0; i < flowerSpawners.Count; i++)
+        {
+            if (flowerSpawners[i].currentBug == null)
+            {
+                availableFlowers.Add(flowerSpawners[i]);
+            }
+        }
+        if (availableFlowers.Count > 0)
+        {
+            int randomIndex = Random.Range(0, availableFlowers.Count);
+            FlowerBugSpawner selectedFlower = availableFlowers[randomIndex];
+            selectedFlower.SpawnRandomBug();
+        }
+    }
+
+    public bool CanSpawnBug()
+    {
+        return currentBugCount < maxBugsInIsland;
+    }
+
+    public void AddBug()
+    {
+        currentBugCount++;
+    }
+
+    public void RemoveBug()
+    {
+        currentBugCount--;
+    }
+}
