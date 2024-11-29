2d321d89306e9936cabef487007c013a2202db7b
Soyoung <soyoung7896@naver.com>
2024-11-29 16:20:32 +0900

    [변경 NPC State, TestDialogMove
    폴더 이동

---
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/TestDialogMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDialogMove : MonoBehaviour
+{
+    MoriController moriCtrl;
+    public float moveSpeed;
+
+    private void Start()
+    {
+        moriCtrl = FindObjectOfType<MoriController>();
+    }
+    private void Update()
+    {
+        MoveTest();
+    }
+
+    private void MoveTest()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(x * moveSpeed * Time.deltaTime, 0, z * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("NPCMori"))
+        {
+            //NPC별 태그 추가해야함
+            moriCtrl.AirplaneDialogStart();
+        }
+    }
+}
