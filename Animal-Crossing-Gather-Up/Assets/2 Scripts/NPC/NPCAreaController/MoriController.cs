27071a57851d2da55eabe6a489a77f0aac6b819d
Soyoung <soyoung7896@naver.com>
2024-11-29 16:20:01 +0900

    [변경] MoriController Script
    npc 모리만 사용하는 스크립트

---
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/NPCAreaController/MoriController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.UI;
+
+public class MoriController : DialogController, IDialog
+{
+
+    public NPCDialogData moriDialogData;
+
+    private void Awake()
+    {
+        moriDialogData.isChooseActive = false;
+        moriDialogData.isEnterActive = false;
+    }
+    protected override void Start()
+    {
+        base.Start();
+        dialogData = moriDialogData;
+        uiManager.firstChooseText.text = "외출할래";
+        uiManager.thirdChooseText.text = "지금은 안할래";
+    }
+
+    public void AirplaneDialogStart()
+    {
+        uiManager.dialogPanel.SetActive(true);
+        DialogStart();
+    }
+
+    public void FirstAccept()
+    {
+        Debug.Log("마일섬 출발");
+        uiManager.choosePanel.SetActive(false);
+        uiManager.dialogPanel.SetActive(false);
+        uiManager.enterPanel.SetActive(false);
+    }
+    public void EndTalk()
+    {
+        Debug.Log("모리와의 대화 종료");
+        uiManager.choosePanel.SetActive(false);
+        uiManager.dialogPanel.SetActive(false);
+        uiManager.enterPanel.SetActive(false);
+    }
+}
