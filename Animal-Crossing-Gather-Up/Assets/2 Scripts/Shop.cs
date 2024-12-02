be0630f1782cdd3893ba3356e4fb41f47e9e6851
JiwonEE2 <jiwonchichi1@gmail.com>
2024-12-02 12:58:31 +0900

    [변경] Shop
    purchaseUI 오류 수정. inventory 열 때 null 수정 전

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Shop.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Shop.cs
@@ -26,13 +26,18 @@ public class Shop : MonoBehaviour
 	// ±¸¸Å
 	private PurchaseUI purchaseUI;
 
+	public GameObject[] horizontalLayoutObjects;
+	public GameObject slotPrefab;   // Prefab for the slot
+
+	private const int slotsPerRow = 2; // Number of slots per row
+
 	private void Start()
 	{
 		purchaseUI = FindObjectOfType<PurchaseUI>();
-		for (int i = 0; i < purchaseSlots.Count; i++)
+		for (int i = 0; i < items.Count; i++)
 		{
-			purchaseSlots[i].Initialize(items[i]);
-			print(purchaseSlots[i]);
+			AddSlot(i < slotsPerRow ? 0 : 1); // Add empty slots based on index
+			purchaseSlots[i].Initialize(items[i]);  // slot initialize
 		}
 	}
 
@@ -48,9 +53,18 @@ public class Shop : MonoBehaviour
 		}
 	}
 
+	private void AddSlot(int verticalCount)
+	{
+		GameObject slotObject = Instantiate(slotPrefab, horizontalLayoutObjects[verticalCount].transform); // Instantiate the slotPrefab
+		Slot slot = slotObject.GetComponent<Slot>();    // Get the slot component
+		purchaseSlots.Add(slot);
+
+		purchaseUI.AddSlotUI(slotObject.GetComponent<SlotUI>());    // Add SlotUI to PurchaseUI
+	}
+
 	public List<Slot> GetPurchaseSlotInfo()
 	{
-		List<Slot> newSlots = new List<Slot>(purchaseSlots);
+		List<Slot> newSlots = purchaseSlots;
 		return newSlots;
 	}
 
