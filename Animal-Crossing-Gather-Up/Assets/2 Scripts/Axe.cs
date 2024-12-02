fe379316aa5b7d07d2584c2dde431a39e46fceb9
evespimrose <evespimrose@github.com>
2024-12-02 10:38:00 +0900

    [추가] Tools
    도구에 커맨드 멤버변수로 추가

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Axe.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Axe.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Axe : Tool
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        collectCommand = new AxeCollectCommand();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Animal-Crossing-Gather-Up/Assets/2 Scripts/FishingRod.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/FishingRod.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class FishingRod : Tool
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        collectCommand = new FishingRodCollectCommand();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
