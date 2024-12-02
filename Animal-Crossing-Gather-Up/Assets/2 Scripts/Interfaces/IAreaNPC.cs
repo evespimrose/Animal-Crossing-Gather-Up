1f0d036403697c4148bbbb61f222263890f324ff
Soyoung <soyoung7896@naver.com>
2024-12-02 13:31:49 +0900

    [변경] IAreaNPC

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Interfaces/IAreaNPC.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Interfaces/IAreaNPC.cs
@@ -4,7 +4,4 @@ using UnityEngine;
 
 public interface IAreaNPC
 {
-    public void FirstAccept(); //첫번째 선택지(선택지 3개일 경우 NPC에서 1개 추가)
-    public void EndTalk(); //대화 종료 선택지'
-    public void InteractionPlayer(); // NPC와의 상호작용
 }
