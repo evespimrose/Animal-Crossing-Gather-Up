e207c15e4bb37fa06ecc91d838a7aab049c6fa71
JiwonEE2 <jiwonchichi1@gmail.com>
2024-12-03 23:16:49 +0900

    [변경] OptionUI
    시작 시 자동으로 패널 false 되도록 수정

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/OptionUI.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/OptionUI.cs
@@ -5,85 +5,91 @@ using UnityEngine;
 
 public class OptionUI : MonoBehaviour
 {
-    [Header("Option Panel")]
-    public GameObject optionPanel; //옵션패널
-    public TextMeshProUGUI[] optionTexts; //옵션 텍스트 
+	[Header("Option Panel")]
+	public GameObject optionPanel; //옵션패널
+	public TextMeshProUGUI[] optionTexts; //옵션 텍스트 
 
-    [Header("Cursor Move")]
-    public GameObject cursor; //커서 이미지
-    public GameObject underline; //밑줄 이미지
-    public int currentIndex; //커서/밑줄이 가리키고 있는 현재 인덱스optionText -> 커서용
-    public string currentOption; //현재 옵션
-    private int optionSize; //옵션 개수
+	[Header("Cursor Move")]
+	public GameObject cursor; //커서 이미지
+	public GameObject underline; //밑줄 이미지
+	public int currentIndex; //커서/밑줄이 가리키고 있는 현재 인덱스optionText -> 커서용
+	public string currentOption; //현재 옵션
+	private int optionSize; //옵션 개수
 
-    public void SetOptions(string[] options)
-    {
-        optionSize = options.Length;
-        for (int i = 0; i < options.Length; i++)
-        {
-            optionTexts[i].text = options[i];
-            optionTexts[i].gameObject.SetActive(true);
-        }
-    }
+	// set active false when start
+	private void Start()
+	{
+		optionPanel.SetActive(false);
+	}
 
-    public void PanelActive(bool isActive)
-    {
-        //option panel 활성화
-        optionPanel.SetActive(isActive);
-        cursor.SetActive(isActive);
-    }
+	public void SetOptions(string[] options)
+	{
+		optionSize = options.Length;
+		for (int i = 0; i < options.Length; i++)
+		{
+			optionTexts[i].text = options[i];
+			optionTexts[i].gameObject.SetActive(true);
+		}
+	}
 
-    public void CursorMove()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            currentIndex--;
-            if (currentIndex <= 0)
-            {
-                currentIndex = 0;
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            currentIndex++;
-            if (currentIndex >= optionSize)
-            {
-                currentIndex = optionSize - 1;
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            SelectOption();
-        }
+	public void PanelActive(bool isActive)
+	{
+		//option panel 활성화
+		optionPanel.SetActive(isActive);
+		cursor.SetActive(isActive);
+	}
 
-        SelecteedOptionPosition();
-    }
+	public void CursorMove()
+	{
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			currentIndex--;
+			if (currentIndex <= 0)
+			{
+				currentIndex = 0;
+			}
+		}
+		else if (Input.GetKeyDown(KeyCode.S))
+		{
+			currentIndex++;
+			if (currentIndex >= optionSize)
+			{
+				currentIndex = optionSize - 1;
+			}
+		}
+		else if (Input.GetKeyDown(KeyCode.Return))
+		{
+			SelectOption();
+		}
 
-    public string SelectOption()
-    {
-        currentOption = optionTexts[currentIndex].text;
-        optionPanel.SetActive(false);
+		SelecteedOptionPosition();
+	}
 
-        print($"현재 옵션: {currentOption}");
-        return currentOption;
-    }
+	public string SelectOption()
+	{
+		currentOption = optionTexts[currentIndex].text;
+		optionPanel.SetActive(false);
+
+		print($"현재 옵션: {currentOption}");
+		return currentOption;
+	}
 
 
-    private void SelecteedOptionPosition()
-    {
-        TextMeshProUGUI selectedOption = optionTexts[currentIndex];
-        float optionWidth = selectedOption.preferredWidth;
+	private void SelecteedOptionPosition()
+	{
+		TextMeshProUGUI selectedOption = optionTexts[currentIndex];
+		float optionWidth = selectedOption.preferredWidth;
 
-        Vector3 cursorPos = selectedOption.transform.position;
-        cursorPos.x += optionWidth + 60;
-        cursor.transform.position = cursorPos;
+		Vector3 cursorPos = selectedOption.transform.position;
+		cursorPos.x += optionWidth + 60;
+		cursor.transform.position = cursorPos;
 
-        Vector3 underlinePos = selectedOption.transform.position;
-        underlinePos.x += optionWidth + 20f;
-        underline.transform.position = underlinePos;
+		Vector3 underlinePos = selectedOption.transform.position;
+		underlinePos.x += optionWidth + 20f;
+		underline.transform.position = underlinePos;
 
-        RectTransform underlineRect = underline.GetComponent<RectTransform>();
-        underlineRect.sizeDelta = new Vector2(optionWidth - 20f, underlineRect.sizeDelta.y);
-    }
+		RectTransform underlineRect = underline.GetComponent<RectTransform>();
+		underlineRect.sizeDelta = new Vector2(optionWidth - 20f, underlineRect.sizeDelta.y);
+	}
 
 }
