5847640e64a80b641040a0c7187c17401c57e737
Soyoung <soyoung7896@naver.com>
2024-12-03 18:40:02 +0900

    [변경] Dialog Controller
    대화창 자유구현 할 수 있게 변경(미완)

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/DialogController.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/DialogController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Unity.VisualScripting;
+using Unity.Android.Types;
 
 
 
@@ -14,6 +15,9 @@ public class DialogController : MonoBehaviour
     protected Coroutine currentCoroutine;
 
     protected NPCDialogData dialogData;
+    protected string[] activeDialogTexts;
+    protected int activeDialogIndex;
+
     protected virtual void Start()
     {
         uiManager = FindObjectOfType<NPCPanelUI>();
@@ -21,59 +25,76 @@ public class DialogController : MonoBehaviour
         uiManager.dialogPanel.SetActive(false);
         optionui.optionPanel.SetActive(false);
         uiManager.enterPanel.SetActive(false);
+
     }
 
-    public void DialogStart(string[] setDialogTexts, int talkCount)
+    public void DialogStart(string[] setDialogTexts, int dialogIndexCount)
     {
-        FirstTextStart(setDialogTexts, talkCount);
-        if (setDialogTexts.Length > 0)
-        {
-            EnterDialog(setDialogTexts, talkCount);
-        }
-
+        dialogData.isChooseActive = false;
+        activeDialogTexts = setDialogTexts;
+        activeDialogIndex = dialogIndexCount;
+        FirstTextStart(setDialogTexts, dialogIndexCount);
     }
 
     public void EndDialog()
     {
         dialogData.isChooseActive = false;
-        for (int i = 0; i < dialogData.talkCount.Length; i++)
+        for (int i = 0; i < dialogData.dialogIndex.Length; i++)
         {
-            dialogData.talkCount[i] = 0;
+            dialogData.dialogIndex[i] = 0;
         }
+        activeDialogTexts = null;
         optionui.optionPanel.SetActive(false);
         uiManager.dialogPanel.SetActive(false);
     }
 
     protected virtual void Update()
     {
-        optionui.PanelActive(dialogData.isChooseActive);
         uiManager.enterPanel.SetActive(dialogData.isEnterActive);
+
+        if (uiManager.dialogPanel.activeSelf && activeDialogTexts != null)
+        {
+            EnterDialog(activeDialogTexts, activeDialogIndex);
+        }
+        //플레이어와 상호작용 이런 식으로 작성 예정
+        //일정 거리 안에 플레이어가 들어왔을 때 r키를 누르면 대화창 활성화 
+        //if(Vector3.Distance(player.position, npc.position) < 5f))
+        //   {
+        //      if(GetKeyDown(KeyCode.R))
+        //      {
+        //          DialogStart();
+        //      }
+        //   }
+
     }
 
-    public void FirstTextStart(string[] SetdialogTexts, int dialogIndex)
+    public void FirstTextStart(string[] SetdialogTexts, int dialogIndexCount)
     {
         if (currentCoroutine == null)
         {
-            string firstText = SetdialogTexts[0];
+            string firstText = SetdialogTexts[dialogData.dialogIndex[dialogIndexCount]];
             currentCoroutine = StartCoroutine(TypingDialog(firstText));
+            dialogData.dialogIndex[dialogIndexCount]++;
         }
     }
 
-    public void EnterDialog(string[] SetdialogTexts, int dialogIndex)
+    public void EnterDialog(string[] setDialogTexts, int dialogIndexCount)
     {
-        if (dialogIndex < SetdialogTexts.Length)
+        if (dialogData.dialogIndex[dialogIndexCount] >= setDialogTexts.Length)
+        {
+            dialogData.isChooseActive = true;
+            activeDialogTexts = null;
+            return;
+        }
+
+        if (dialogData.dialogIndex[dialogIndexCount] < setDialogTexts.Length && currentCoroutine == null)
         {
             if (Input.GetKeyDown(KeyCode.Space) && currentCoroutine == null)
             {
-                string text = SetdialogTexts[dialogIndex];
+                string text = setDialogTexts[dialogData.dialogIndex[dialogIndexCount]];
                 currentCoroutine = StartCoroutine(TypingDialog(text));
+                dialogData.dialogIndex[dialogIndexCount]++;
             }
-        }
-
-        if (dialogIndex >= SetdialogTexts.Length)
-        {
-            dialogData.isChooseActive = true;
-            dialogIndex = 0;
         }
     }
 
