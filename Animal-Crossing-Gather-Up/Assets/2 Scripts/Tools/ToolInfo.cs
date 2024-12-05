76df69574f923350cf801f78fb8bd94ded962d51
JiwonEE2 <jiwonchichi1@gmail.com>
2024-12-05 21:41:09 +0900

    [변경] ToolInfo
    OnEnable 추가

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Tools/ToolInfo.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Tools/ToolInfo.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewTool", menuName = "Items/Tool/Tool")]
 public class ToolInfo : Item
 {
-    public int maxDurability;
-    public int currentDurability;
-    public bool isEquipped;
-    public ICollectCommand collectCommand;
-} 
+	public int maxDurability;
+	public int currentDurability;
+	public bool isEquipped;
+	public ICollectCommand collectCommand;
+
+	private void OnEnable()
+	{
+		currentDurability = maxDurability;
+		isEquipped = false;
+	}
+}
