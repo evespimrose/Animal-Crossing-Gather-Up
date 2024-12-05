db9865c54810925dfd5968b3f3bcc27b70f2f69a
Soyoung <soyoung7896@naver.com>
2024-12-05 10:28:48 +0900

    [변경] NPC Interaction
    R키 중복으로 누르는 거 제한

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/NPCInteraction.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/NPCInteraction.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class NPCInteraction : MonoBehaviour
 {
     public Transform player;
-    private float interactionDistance = 2f;
+    private float interactionDistance = 4f;
 
-    private Dictionary<Transform, INPCArea> npcInterfaces = new Dictionary<Transform, INPCArea>();
+    private Dictionary<Transform, INPCArea> npcs = new Dictionary<Transform, INPCArea>();
+
+    [HideInInspector]
+    public bool isDialogActive = false;
 
     private void Awake()
     {
         foreach (var npc in GetComponentsInChildren<INPCArea>())
         {
             Transform npcTransform = ((MonoBehaviour)npc).transform;
-            npcInterfaces.Add(npcTransform, npc);
+
+            npcs.Add(npcTransform, npc);
         }
     }
 
@@ -27,10 +31,11 @@ public class NPCInteraction : MonoBehaviour
 
 
         INPCArea nearestNPC = FindNearestNPC();
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && !isDialogActive)
         {
             if (nearestNPC != null)
             {
+                isDialogActive = true;
                 nearestNPC.NPCDialogStart();
             }
         }
@@ -42,13 +47,13 @@ public class NPCInteraction : MonoBehaviour
         float minDistance = interactionDistance;
         INPCArea nearestNPC = null;
 
-        foreach (var npcPair in npcInterfaces)
+        foreach (var npc in npcs)
         {
-            float distance = Vector3.Distance(player.transform.position, npcPair.Key.position);
+            float distance = Vector3.Distance(player.transform.position, npc.Key.position);
             if (distance < minDistance)
             {
                 minDistance = distance;
-                nearestNPC = npcPair.Value;
+                nearestNPC = npc.Value;
             }
         }
 
@@ -57,6 +62,7 @@ public class NPCInteraction : MonoBehaviour
             print("상호작용 가능한 거리");
         }
 
+        print(nearestNPC);
         return nearestNPC;
     }
 }
