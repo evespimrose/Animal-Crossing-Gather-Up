997a93853770015fdb7322a7a6666b34b1d86e2b
primrose <bigjht34@gmail.com>
2024-12-06 00:34:52 +0900

    [변경] Flower
    꽃 수집 초안

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Collectable/Flower.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Collectable/Flower.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flower: MonoBehaviour
+public class Flower: MonoBehaviour, ICollectable
 {
     private FlowerInfo flowerInfo;
 
+    public void Initialize(FlowerInfo info)
+    {
+        flowerInfo = info;
+    }
+
     public void Collect()
     {
+        if (flowerInfo == null) return;
+
         Debug.Log("Flower collected.");
-        GameManager.Instance.inventory.AddItem(flowerInfo);
+
+        GameManager.Instance.player.Collect(flowerInfo);
+        Destroy(gameObject);
     }
 }
