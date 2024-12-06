2ae885cdb02919daa976f1a0e2b6b10892b01603
JiwonEE2 <jiwonchichi1@gmail.com>
2024-12-06 16:28:18 +0900

    [변경] MoneyUI
    열리는 Panel에 따라 MoneyPanel 다른 위치에 활성화되도록 구현

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/UI/MoneyUI.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/UI/MoneyUI.cs
@@ -6,16 +6,34 @@ using UnityEngine;
 public class MoneyUI : MonoBehaviour
 {
 	private int money;
+	public GameObject moneyPanel;
 	public TextMeshProUGUI moneyText;
+	private RectTransform panelTransform;
 
 	private void Start()
 	{
 		moneyText = GetComponentInChildren<TextMeshProUGUI>();
+		panelTransform = moneyPanel.GetComponent<RectTransform>();
+		moneyPanel.SetActive(false);
 	}
 
 	private void Update()
 	{
-		money = FindObjectOfType<Inventory>().money;
-		moneyText.text = money.ToString();
+		if (moneyPanel.activeSelf)
+		{
+			money = FindObjectOfType<Inventory>().money;
+			moneyText.text = money.ToString();
+		}
+	}
+
+	public void ShowMoney(Vector2 vector2)
+	{
+		panelTransform.localPosition = vector2;
+		moneyPanel.SetActive(true);
+	}
+
+	public void HideMoney()
+	{
+		moneyPanel.SetActive(false);
 	}
 }
