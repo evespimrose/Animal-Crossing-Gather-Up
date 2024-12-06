18e2a2f3dc58279baec50cb8e425c2d91851ed62
Soyoung <soyoung7896@naver.com>
2024-12-06 19:02:47 +0900

    [변경] Mori State Controller
    state 구현

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/NPC/StateControl/MoriStateController.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/NPC/StateControl/MoriStateController.cs
@@ -6,31 +6,42 @@ using UnityEngine;
 
 public class MoriStateController : NPCState
 {
+    private Quaternion originalRotation;
 
     private void Awake()
     {
+
     }
     protected override void Start()
     {
         base.Start();
         moveSpeed = 0f;
+        originalRotation = this.transform.rotation;
         SetCurrentState(NPCStateType.LookAround);
     }
     protected override void Update()
     {
         base.Update();
         print($"현재 모리 상태: {npcState.ToString()}");
+
     }
 
     protected override void Talk()
     {
-        base.Talk();
+        if (UIManager.Instance.dialogUI.dialogPanel.activeSelf)
+        {
+            base.Talk();
+        }
+
         if (!UIManager.Instance.dialogUI.dialogPanel.activeSelf)
         {
-            Quaternion.LookRotation(Vector3.forward, Vector3.up);
             anim.SetBool("Talk", false);
-            Task.Delay(2000);
-            SetCurrentState(NPCStateType.LookAround);
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotation, rotateToOriginalSpeed * Time.deltaTime);
+            if (Quaternion.Angle(transform.rotation, originalRotation) < 0.1f)
+            {
+                SetCurrentState(NPCStateType.LookAround);
+            }
+
         }
     }
 
