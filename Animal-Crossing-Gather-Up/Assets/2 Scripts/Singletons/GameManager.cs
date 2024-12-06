bc308fb1b287a3a99f8c968da4379d171d3f475e
evespimrose <evespimrose@github.com>
2024-12-06 15:41:04 +0900

    [변경] GameManager
    머지 오류 正常化

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Singletons/GameManager.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Singletons/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using static BugInfo;
+using System.Linq;
 
 public class GameManager : SingletonManager<GameManager>
 {
