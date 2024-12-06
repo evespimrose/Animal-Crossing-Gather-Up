d143709f37670703557312b33788f07044c40c17
JiwonEE2 <jiwonchichi1@gmail.com>
2024-12-06 13:38:49 +0900

    [변경] MoneyUI
    money 출력되도록 구현

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/UI/MoneyUI.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/UI/MoneyUI.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MoneyUI : MonoBehaviour
 {
 	private int money;
+	public TextMeshProUGUI moneyText;
 
 	private void Start()
 	{
-		//money=
+		money = FindObjectOfType<Inventory>().money;
+		moneyText = GetComponentInChildren<TextMeshProUGUI>();
+	}
+
+	private void Update()
+	{
+		moneyText.text = money.ToString();
 	}
 }
