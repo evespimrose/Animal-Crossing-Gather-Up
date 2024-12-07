ae2b5f37f35d5058c9067c5fe3a8a6ec4f42b410
JiwonEE2 <jiwonchichi1@gmail.com>
2024-12-07 21:55:07 +0900

    [변경] SellUI
    InventoryUI 상속 삭제

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/UI/SellUI.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/UI/SellUI.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SellUI : InventoryUI
+public class SellUI : MonoBehaviour
 {
+	public Inventory inventory;
 	public GameObject sellPanel;
+
+	private List<Slot> slots;
+	private List<SlotUI> slotUIs = new List<SlotUI>();
+	private int cursorOnSlotIndex = 0;
+	private const int slotsPerRow = 10; // Number of slots per row
+	private const int totalRows = 2;    // Total number of rows
 
 	private void Start()
 	{
+		inventory = FindObjectOfType<Inventory>();
 		sellPanel.SetActive(false);
 	}
 
@@ -19,10 +27,69 @@ public class SellUI : InventoryUI
 		}
 	}
 
-	protected override void SelectSlot(int index)
+	private void HandleSlotSelection()
+	{
+		int previousSlotIndex = cursorOnSlotIndex;  // Store the previous index
+
+		if (Input.GetKeyDown(KeyCode.W) && cursorOnSlotIndex >= slotsPerRow)
+		{
+			// Move cursor up
+			cursorOnSlotIndex -= slotsPerRow;   // Move up by one row
+		}
+		else if (Input.GetKeyDown(KeyCode.S) && cursorOnSlotIndex < slotsPerRow * (totalRows - 1))
+		{
+			// Move cursor down
+			cursorOnSlotIndex += slotsPerRow;   // Move down by one row
+		}
+		else if (Input.GetKeyDown(KeyCode.A) && cursorOnSlotIndex % slotsPerRow > 0)
+		{
+			// Move cursor left
+			cursorOnSlotIndex--;    // Move left by one slot
+		}
+		else if (Input.GetKeyDown(KeyCode.D) && cursorOnSlotIndex % slotsPerRow < slotsPerRow - 1)
+		{
+			// Move cursor right
+			cursorOnSlotIndex++;    // Move right by one slot
+		}
+		else if (Input.GetKeyDown(KeyCode.Return))
+		{
+			// Select CursorOnSlot
+			SelectSlot(cursorOnSlotIndex);
+		}
+
+		// Only update the selection if it has changed
+		if (previousSlotIndex != cursorOnSlotIndex)
+		{
+			CursorOnSlot(cursorOnSlotIndex);
+		}
+	}
+
+	private void CursorOnSlot(int index)
+	{
+		// Decursor on all slots
+		foreach (SlotUI slotUI in slotUIs)
+		{
+			slotUI.CursorOnSlotDisplayBackground(false);
+			slotUI.CursorOnSlotDisplayName(false);
+		}
+
+		// Cursor on the current slot
+		slotUIs[index].CursorOnSlotDisplayBackground(true);
+		if (slots[index].Item != null)
+		{
+			slotUIs[index].CursorOnSlotDisplayName(true);
+		}
+	}
+
+	private void SelectSlot(int index)
 	{
 		slots[index].isSelected = true != false;
-		slotUIs[index].SelectSlotAtSell(!slots[index].isSelected);
+		slotUIs[index].SelectSlotAtSell(slots[index].isSelected);
+	}
+
+	public void AddSlotUI(SlotUI slotUI)
+	{
+		slotUIs.Add(slotUI);
 	}
 
 	public void SellPanelOpen()
@@ -46,6 +113,18 @@ public class SellUI : InventoryUI
 		{
 			UIManager.Instance.HideMoney();
 			sellPanel.SetActive(false);
+		}
+	}
+
+	public void UpdateAllSlotUIs()
+	{
+		// 인벤토리의 Slot의 정보들을 새로 할당받아서 받아옴
+		// 이 slots를 변경해도 Inventory의 slot에 담긴 아이템이 바뀌지는 않음
+		slots = inventory.GetSlotInfo();
+		// 인벤토리의 Slot의 정보를 토대로 slotUIs를 업데이트
+		for (int i = 0; i < slots.Count; i++)
+		{
+			slotUIs[i].UpdateUI(slots[i].Item, slots[i].stackCount);
 		}
 	}
 }
