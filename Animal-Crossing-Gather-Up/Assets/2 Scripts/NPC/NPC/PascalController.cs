3782e3f99e60e359a951e952882cccba0988fe1f
장소영 <soyoung7896@naver.com>
2024-12-09 00:24:12 +0900

    [변경] NPC Controller
    파스칼, 리즈 대화 추가

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/NPC/PascalController.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/NPC/PascalController.cs
@@ -2,17 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PascalController : MonoBehaviour
+public class PascalController : DialogController, INPCDialog
 {
-    // Start is called before the first frame update
-    void Start()
+    public NPCDialogData pascalDialogData;
+
+    private void Awake()
     {
-        
+        pascalDialogData.isChooseActive = false;
+        pascalDialogData.isEnterActive = false;
+        for (int i = 0; i < pascalDialogData.dialogIndex.Length; i++)
+        {
+            pascalDialogData.dialogIndex[i] = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Start()
     {
-        
+        base.Start();
+        dialogData = pascalDialogData;
+        pascalDialogData.currentOption = "";
+        pascalDialogData.currentOption = UIManager.Instance.optionUI.currentOption;
+        string[] pascalOptions = { "파스칼 테스트" };
+        UIManager.Instance.optionUI.SetOptions(pascalOptions);
+    }
+
+    protected override void Update()
+    {
+
+        base.Update();
+        if (pascalDialogData.currentOption != UIManager.Instance.optionUI.currentOption && UIManager.Instance.optionUI.currentOption != null)
+        {
+            pascalDialogData.currentOption = UIManager.Instance.optionUI.currentOption;
+            SelectedOptionAfter();
+        }
+    }
+
+    public void NPCDialogStart()
+    {
+        string[] pascalOptions = { "파스칼 테스트" };
+        UIManager.Instance.optionUI.SetOptions(pascalOptions);
+        UIManager.Instance.dialogUI.dialogPanel.SetActive(true);
+        DialogStart(pascalDialogData.dialogTexts, pascalDialogData.dialogIndex[0]);
+        print("파스칼 대화 시작");
+    }
+
+    public void SelectedOptionAfter()
+    {
+        if (pascalDialogData.currentOption == "파스칼 테스트")
+        {
+            dialogData.isChooseActive = false;
+            UIManager.Instance.optionUI.PanelActive(pascalDialogData.isChooseActive);
+
+            DialogStart(pascalDialogData.nextDialogTexts, pascalDialogData.dialogIndex[1]);
+
+            string[] pascalOptions = { "파스칼 테스트 끝" };
+            UIManager.Instance.optionUI.SetOptions(pascalOptions);
+        }
+
+        //else if (pascalDialogData.currentOption == "test2")
+        //{
+        //    dialogData.isChooseActive = false;
+        //    UIManager.Instance.optionUI.PanelActive(pascalDialogData.isChooseActive);
+
+        //    DialogStart(pascalDialogData.thirdDialogTexts, pascalDialogData.dialogIndex[2]);
+
+        //    string[] roadriOptions = { "테스트 2 끝" };
+        //    UIManager.Instance.optionUI.SetOptions(roadriOptions);
+        //}
+
+        else if (pascalDialogData.currentOption == "파스칼 테스트 끝")
+        {
+            EndDialog();
+            print("파스칼 테스트 완료");
+        }
+
+        //else if (pascalDialogData.currentOption == "테스트 2 끝")
+        //{
+        //    EndDialog();
+        //    print("데이지 2 옵션 테스트 완료");
+        //}
     }
 }
--- Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/NPC/ReeseController.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/NPC/NPC/ReeseController.cs
@@ -2,7 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReeseController : DialogController
+public class ReeseController : DialogController, INPCDialog
 {
+    public NPCDialogData reeseDialogData;
 
+    private void Awake()
+    {
+        reeseDialogData.isChooseActive = false;
+        reeseDialogData.isEnterActive = false;
+        for (int i = 0; i < reeseDialogData.dialogIndex.Length; i++)
+        {
+            reeseDialogData.dialogIndex[i] = 0;
+        }
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        dialogData = reeseDialogData;
+        reeseDialogData.currentOption = "";
+        reeseDialogData.currentOption = UIManager.Instance.optionUI.currentOption;
+        string[] reeseOptions = { "리즈 테스트" };
+        UIManager.Instance.optionUI.SetOptions(reeseOptions);
+    }
+
+    protected override void Update()
+    {
+
+        base.Update();
+        if (reeseDialogData.currentOption != UIManager.Instance.optionUI.currentOption && UIManager.Instance.optionUI.currentOption != null)
+        {
+            reeseDialogData.currentOption = UIManager.Instance.optionUI.currentOption;
+            SelectedOptionAfter();
+        }
+    }
+
+    public void NPCDialogStart()
+    {
+        string[] reeseOptions = { "리즈 테스트" };
+        UIManager.Instance.optionUI.SetOptions(reeseOptions);
+        UIManager.Instance.dialogUI.dialogPanel.SetActive(true);
+        DialogStart(reeseDialogData.dialogTexts, reeseDialogData.dialogIndex[0]);
+        print("리즈 대화 시작");
+    }
+
+    public void SelectedOptionAfter()
+    {
+        if (reeseDialogData.currentOption == "리즈 테스트")
+        {
+            dialogData.isChooseActive = false;
+            UIManager.Instance.optionUI.PanelActive(reeseDialogData.isChooseActive);
+
+            DialogStart(reeseDialogData.nextDialogTexts, reeseDialogData.dialogIndex[1]);
+
+            string[] reeseOptions = { "리즈 테스트 끝" };
+            UIManager.Instance.optionUI.SetOptions(reeseOptions);
+        }
+
+        //else if (pascalDialogData.currentOption == "test2")
+        //{
+        //    dialogData.isChooseActive = false;
+        //    UIManager.Instance.optionUI.PanelActive(pascalDialogData.isChooseActive);
+
+        //    DialogStart(pascalDialogData.thirdDialogTexts, pascalDialogData.dialogIndex[2]);
+
+        //    string[] roadriOptions = { "테스트 2 끝" };
+        //    UIManager.Instance.optionUI.SetOptions(roadriOptions);
+        //}
+
+        else if (reeseDialogData.currentOption == "리즈 테스트 끝")
+        {
+            EndDialog();
+            print("리즈 테스트 완료");
+        }
+
+        //else if (pascalDialogData.currentOption == "테스트 2 끝")
+        //{
+        //    EndDialog();
+        //    print("데이지 2 옵션 테스트 완료");
+        //}
+    }
 }
