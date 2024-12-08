1709c89d309810d865f7989582e24411c9246299
RJH7387 <abj2563@naver.com>
2024-12-09 02:35:56 +0900

    [추가] Weather
    비,눈 프리펩 마테리얼 텍스쳐 추가

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/DateWeatherSeason/WeatherSystem.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/DateWeatherSeason/WeatherSystem.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class WeatherSystem : MonoBehaviour
 {
-    [Header("³¯¾¾ È¿°ú")]
-    [SerializeField] private ParticleSystem rainEffect;
-    [SerializeField] private ParticleSystem snowEffect;
+    [Header("³¯¾¾ È¿°ú ÇÁ¸®ÆÕ")]
+    [SerializeField] private GameObject rainPrefab;
+    [SerializeField] private GameObject snowPrefab;
 
     [Header("³¯¾¾ ¼³Á¤")]
     [SerializeField] private float weatherCheckInterval = 1800f;  // 30ºÐ¸¶´Ù Ã¼Å©
@@ -15,10 +15,18 @@ public class WeatherSystem : MonoBehaviour
     private float nextWeatherCheck;
     private SeasonSystem seasonSystem;
 
+    private GameObject currentRainInstance;
+    private GameObject currentSnowInstance;
+
     private void Start()
     {
         seasonSystem = FindObjectOfType<SeasonSystem>();
         nextWeatherCheck = TimeManager.Instance.CurrentTime + weatherCheckInterval;
+    
+        currentRainInstance = Instantiate(rainPrefab, transform);
+        currentSnowInstance = Instantiate(snowPrefab, transform);
+        currentRainInstance.SetActive(false);
+        currentSnowInstance.SetActive(false);
     }
 
     private void Update()
@@ -37,18 +45,18 @@ public class WeatherSystem : MonoBehaviour
         switch (seasonSystem.CurrentSeason)
         {
             case SeasonSystem.Season.Summer:
-                rainEffect.gameObject.SetActive(random <= precipitationChance);
-                snowEffect.gameObject.SetActive(false);
+                currentRainInstance.SetActive(random <= precipitationChance);
+                currentSnowInstance.SetActive(false);
                 break;
 
             case SeasonSystem.Season.Winter:
-                snowEffect.gameObject.SetActive(random <= precipitationChance);
-                rainEffect.gameObject.SetActive(false);
+                currentSnowInstance.SetActive(random <= precipitationChance);
+                currentRainInstance.SetActive(false);
                 break;
 
             default: // º½, °¡À»
-                rainEffect.gameObject.SetActive(random <= precipitationChance * 0.5f);
-                snowEffect.gameObject.SetActive(false);
+                currentRainInstance.SetActive(random <= precipitationChance * 0.5f);
+                currentSnowInstance.SetActive(false);
                 break;
         }
     }
