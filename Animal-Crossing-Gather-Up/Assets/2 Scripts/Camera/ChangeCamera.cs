d56f53c2dd564d570388a3bc8058cb3951c0e0b3
장소영 <soyoung7896@naver.com>
2024-12-08 22:56:22 +0900

    [변경] Change Camera

--- Animal-Crossing-Gather-Up/Assets/2 Scripts/Camera/ChangeCamera.cs
+++ Animal-Crossing-Gather-Up/Assets/2 Scripts/Camera/ChangeCamera.cs
@@ -6,19 +6,25 @@ using Cinemachine;
 public class ChangeCamera : MonoBehaviour
 {
     public Camera mainCamera; //메인 카메라
-    public float changeFOV = 25f; //확대할 FOV
-    public float originalFOV = 40f; //기존 FOV
-    public float duration = 0.5f; 
-    public float yValue = 1.5f; 
+    private MMFCamera mmfCamera;
+    private float changeFOV = 15f; //확대할 FOV
+    private float originalFOV = 40f; //기존 FOV
+    private float duration = 0.5f;
+
+    private void Start()
+    {
+        mmfCamera = GetComponent<MMFCamera>();
+        mmfCamera.currentY = mmfCamera.transform.position.y;
+    }
 
     public void ZoomIn(Transform playerTransform)
     {
-        StartCoroutine(ZoomCoroutine(changeFOV, yValue, playerTransform));
+        StartCoroutine(ZoomCoroutine(changeFOV, 8f, playerTransform));
     }
 
     public void ZoomOut(Transform playerTransform)
     {
-        StartCoroutine(ZoomCoroutine(originalFOV, -yValue, playerTransform)); //y값 조정 안되는 중
+        StartCoroutine(ZoomCoroutine(originalFOV, -8f, playerTransform)); //y값 조정 안되는 중
     }
 
     private IEnumerator ZoomCoroutine(float targetFOV, float yValue, Transform playerTransform)
@@ -33,13 +39,13 @@ public class ChangeCamera : MonoBehaviour
             elapsedTime += Time.deltaTime;
             mainCamera.fieldOfView = Mathf.Lerp(currentFOV, targetFOV, elapsedTime / duration);
 
-            mainCamera.transform.position = new Vector3(currentPos.x, playerTransform.position.y + yValue * (elapsedTime / duration), currentPos.z);
-
+            mainCamera.transform.position = new Vector3(currentPos.x, mainCamera.transform.position.y + yValue * (elapsedTime / duration), currentPos.z);
+            mmfCamera.currentY += yValue * (Time.deltaTime / duration);
             yield return null;
         }
 
         mainCamera.fieldOfView = targetFOV;
-        mainCamera.transform.position = new Vector3(currentPos.x, playerTransform.position.y + yValue, currentPos.z); 
+        mainCamera.transform.position = new Vector3(currentPos.x, mainCamera.transform.position.y + yValue, currentPos.z); 
     }
 
 }
